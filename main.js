491897cbe0063847996b511aac8ba17dfb35aa6d
Ayres Roux <rouxltda@gmail.com>
2024-05-23 19:42:47 -0300

    aplicando eventos e callback

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
+const screen1 = document.querySelector(".screen-1");
+const screen2 = document.querySelector(".screen-2");
+
 const randomNumber = Math.round(Math.random() * 10);
 let xAttempts = 1;
 
-function handleClick(event) {
+function handleTryClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
 
   if (Number(inputNumber.value) == randomNumber) {
-    document.querySelector(".screen-1").classList.add("hide");
-    document.querySelector(".screen-2").classList.remove("hide");
+    screen1.classList.add("hide");
+    screen2.classList.remove("hide");
 
     document.querySelector(
       ".screen-2 h2"
     ).innerHTML = `Acertou em ${xAttempts} tentativas`;
   }
 
+  inputNumber.value = "";
+
   xAttempts++;
 }
+
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
+btnTry.addEventListener("click", handleTryClick);
+btnReset.addEventListener("click", function () {
+  screen1.classList.remove("hide");
+  screen2.classList.add("hide");
+
+  xAttempts = 1;
+});
