25f6bad7da52ccbe4f60dac9e9ded1c565afeaa5
Ayres Roux <rouxltda@gmail.com>
2024-05-23 19:04:25 -0300

    aplicando a lógica

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
+const randomNumber = Math.round(Math.random() * 10);
+let xAttempts = 1;
+
 function handleClick(event) {
   event.preventDefault();
 
   const inputNumber = document.querySelector("#inputNumber");
 
-  console.log(inputNumber.value);
+  if (Number(inputNumber.value) == randomNumber) {
+    document.querySelector(".screen-1").classList.add("hide");
+    document.querySelector(".screen-2").classList.remove("hide");
+
+    document.querySelector(
+      ".screen-2 h2"
+    ).innerHTML = `Acertou em ${xAttempts} tentativas`;
+  }
+
+  xAttempts++;
 }
